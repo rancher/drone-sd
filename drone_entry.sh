168421cb66ebf7e6b614b97b143c2ef5c08538f4
Bill Maxwell <bill@rancher.com>
2015-08-10 12:59:11 -0700

    Check for workers before continuing to load

--- drone_entry.sh
+++ drone_entry.sh
@@ -7,13 +7,16 @@ WORKERS=${WORKERS:-2}
 
 if [ ! -z ${WORKER_DNS} ]; then
   workers=
-  for ip in $(dig ${WORKER_DNS}|grep ^${WORKER_DNS}\. |awk '{print $5}'); do
-    for i in $(seq 1 ${WORKERS});do
-        workers=${workers}tcp://$ip:2375,
+  while [ -z ${workers} ]; do
+    echo "Checking for ${WORKER_DNS} entries..."
+    for ip in $(dig ${WORKER_DNS}|grep ^${WORKER_DNS}\. |awk '{print $5}'); do
+        for i in $(seq 1 ${WORKERS});do
+            workers=${workers}tcp://$ip:2375,
+        done
     done
+    workers=$(echo $workers|sed "s/,$//")
+    export DRONE_WORKER_NODES=$workers
   done
-  workers=$(echo $workers|sed "s/,$//")
-  export DRONE_WORKER_NODES=$workers
 fi
 
 
